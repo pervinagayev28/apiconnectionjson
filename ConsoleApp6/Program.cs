b8987aec17488583fae4d1e0912ee748f2cc3162
Agayev Pervin Nadir <Agaye_jz58@itstep.edu.az>
2023-09-27 17:49:33 +0400

    ...

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await foo();
+        }
+
+        static async Task foo()
+        {
+            List<data> datas = new List<data>();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string gistUrl = "https://api.first.org/data/v1/countries";
+                string json = await httpClient.GetStringAsync(gistUrl);
+                datas = JsonSerializer.Deserialize<List<data>>(json);
+            }
+            foreach (dynamic item in datas)
+            {
+                Console.WriteLine(item.name);
+            }
+        }
+    }
+
+    class data
+    {
+        public string country { get; set; }
+        public string region { get; set; }
+    }
+}
